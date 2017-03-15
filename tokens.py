90a812bf3be17d8d977515e70501f04e42c37c17
116dariya <116dariya@gmail.com>
2017-03-15 11:32:17 +0600

    Add files via upload

---
+++ tokens.py
@@ -0,0 +1,7 @@
+https://oauth.vk.com/authorize?client_id=5925812&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,status&response_type=token&v=5.52
+https://oauth.vk.com/authorize?client_id=5925812&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,&response_type=token&v=5.52
+token = 0a130915e6c4109048830ff1cc898cd4d4909e4d58baf5b4ac63268902b445ca19f478ae85b3abd1aa585
+token = 'https://oauth.vk.com/blank.html#access_token=0a130915e6c4109048830ff1cc898cd4d4909e4d58baf5b4ac63268902b445ca19f478ae85b3abd1aa585&expires_in=86400&user_id=162765493'
+https://api.vk.com/method/friends.delete?user_id=257542059&text=bye&v=5.52&access_token=0a130915e6c4109048830ff1cc898cd4d4909e4d58baf5b4ac63268902b445ca19f478ae85b3abd1aa585
+https://api.vk.com/method/friends.get?order=hints&fields=photo_100,v=5.52&access_token=0a130915e6c4109048830ff1cc898cd4d4909e4d58baf5b4ac63268902b445ca19f478ae85b3abd1aa585
+tt = 0bfb458ccd40fc5fbc12bd95db5b4480fbb3162242b79e8b302e3780c82bdbc7a9bdb355344ecff00e86d
